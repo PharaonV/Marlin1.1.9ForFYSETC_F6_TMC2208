29172675598a0966038cfc5e3e672ac0b053924f
Geroge Fu <nailao_5918@outlook.com>
2018-09-07 15:49:34 +0800

    Upload local file

---
+++ output/libraries/TMC2208Stepper/source/TMC2208Stepper.cpp.d
@@ -0,0 +1,34 @@
+D:\Documents\Arduino\projects\Marlin-1.1.9_2208\output\libraries\TMC2208Stepper\source\TMC2208Stepper.cpp.o: \
+ D:\Documents\Arduino\libraries\TMC2208Stepper\src\source\TMC2208Stepper.cpp \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Documents\Arduino\libraries\TMC2208Stepper\src/TMC2208Stepper.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\George\AppData\Local\Arduino15\packages\fysetcf6\hardware\avr\1.3.0\variants\fysetcf6/pins_arduino.h \
+ D:\Documents\Arduino\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ D:\Documents\Arduino\libraries\TMC2208Stepper\src\source\TMC2208Stepper_MACROS.h \
+ D:\Documents\Arduino\libraries\TMC2208Stepper\src\source\../TMC2208Stepper_REGDEFS.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\serial.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\MarlinConfig.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\fastio.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\macros.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\fastio_1280.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\boards.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\Version.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\Configuration.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\Conditionals_LCD.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\tmc_macros.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\Configuration_adv.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\pins.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\pins_I3PLUS3030.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\Conditionals_post.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\SanityCheck.h \
+ D:\Documents\Arduino\projects\Marlin-1.1.x\ _fys\Marlin\MarlinSerial.h
